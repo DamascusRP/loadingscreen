3888ddf3a778241f411d901ad05abba1a88c454d
cmdblox <94880442+cmdblox@users.noreply.github.com>
2024-07-31 22:47:19 -0700

    add files

---
+++ api/screenGetLive.php
@@ -0,0 +1,41 @@
+[
+  1,
+  0,
+  [
+    {
+      "Id": 10821,
+      "Display_Id": "9d0609a0",
+      "User_Id": 33336,
+      "Created": "2024-08-01 04:59:19",
+      "Last_Edit": "2024-08-01 05:39:00",
+      "Data_Background": "{\"background-images\":[\"-2\"],\"background-settings\":{\"blur\":0,\"shuffle\":0,\"darken\":0,\"delay\":\"5.00\"}}",
+      "Data_Music": "{\"music-files\":[],\"music-volume\":[],\"music-settings\":{\"shuffle\":0}}",
+      "Data_Layout": "{\"layout\":\"1\"}",
+      "Data_Content": "{\"one-line-text\":[\"Damascus DarkRP\",\"Your Activity\",\"Admin Team\",\"Info\"],\"multi-line-text\":[\"Thank you for joining Damascus DarkRP!\"],\"image\":[\"-1\"],\"rules-text\":[\"Read the rules at: damascusrp.github.io\",\"Join the discord at: discord.gg/hQvXP5Sk\"],\"staff-steamid-text\":[\"76561198821285857\", \"76561199122330961\"],\"staff-role-text\":[\"Owner\",\"Owner\",\"Moderator\",\"Senior-Support\",\"Support\"]}",
+      "Data_Customize": "{\"font-family\":\"9\",\"design-mode\":\"0\",\"color-mode\":1,\"primary-color\":\"#a01414\",\"font-color\":\"#ffffff\",\"border-radius\":\"50.00\",\"shadow\":\"2.00\",\"screen-scale\":\"100.00\"}",
+      "Status": 1,
+      "Pro": 1,
+      "Font_Info": [
+        "Poppins",
+        "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      ],
+      "Background_Info": [
+        [
+          -2,
+          "../wallpaper.png",
+          "wallpaper.jpg"
+        ]
+      ],
+      "Image_Info": {
+      },
+      "Music_Info": [],
+      "Version": 1,
+      "Views": 0,
+      "Logo": "../icon.png",
+      "LogoLow": "../icon.png",
+      "LogoGrey": "../icon.png",
+      "Description": "Uses: Plain",
+      "Layout_File": "https://layouts.gmod-lsm.com/v2/plain.html"
+    }
+  ]
+]
